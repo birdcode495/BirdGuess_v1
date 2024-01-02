4d0e279f91a7fff45f4a5f6f47d8f88fa0d78ec3
Geovanny <birdcode.py@gmail.com>
2024-01-02 15:12:19 -0500

    Project start-up

---
+++ Welcome.py
@@ -0,0 +1,10 @@
+# Training program in python to guessing bird names
+
+# Learn how to allow the user to enter a message and save it in a variable
+
+# Ask the user to put down his name
+
+username = input("Enter your name: ")
+
+print("Hello ", username, "welcome to BirdCode!!")
+
