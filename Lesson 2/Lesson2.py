ea24677e83fd2da325a1b46e8e4bb1084bc9ef7f
Geovanny <birdcode.py@gmail.com>
2024-06-06 13:51:56 -0500

    Creacion de una funcion para rerpoducir audio de sonido ambiental

---
+++ Lesson 2/Lesson2.py
@@ -0,0 +1,26 @@
+
+# ----------------------------------  Python functions creation -----------------------------------------------------
+
+## -------------------------- Importar la librería playsound()
+
+from playsound import playsound
+
+rutaCantoAve = "Uranomitra_franciae.wav"
+
+
+def UranoCanto():
+
+	print("Reproduciendo el sonido ambiental")
+	playsound(rutaCantoAve)
+
+
+UranoCanto()
+
+
+
+
+
+
+
+
+
